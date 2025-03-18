5d70a51c807f24a21af0afeba8af6040b7a469a8
Luo-Zhii <anhduc20053110@gmail.com>
2025-03-18 13:40:42 +0700

    asynch with backend

--- src/components/admin/company/modal.company.tsx
+++ src/components/admin/company/modal.company.tsx
@@ -124,9 +124,9 @@ const ModalCompany = (props: IProps) => {
     };
 
     const beforeUpload = (file: any) => {
-        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/webp';
         if (!isJpgOrPng) {
-            message.error('You can only upload JPG/PNG file!');
+            message.error('You can only upload JPG/PNG/WEBP file!');
         }
         const isLt2M = file.size / 1024 / 1024 < 2;
         if (!isLt2M) {
